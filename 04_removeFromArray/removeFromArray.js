2975e6e0d7994a74593b8feeb061263afe6e3cd7
Devin Beck <devin6beck@gmail.com>
2022-02-03 08:47:38 -0700

    complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function(array, itemToRemove) {
-  array.splice(array.indexOf(itemToRemove), 1)
-  return array;
+const removeFromArray = function(...args) {
+  // The first of the args will be the array so we pull it out here.
+  const array = args[0]; 
+
+  const newArray = [];
+
+  array.forEach((item) => {
+    if (!args.includes(item)) {
+      newArray.push(item);
+    }
+  });
+  return newArray;
 };
 
 // Do not edit below this line
