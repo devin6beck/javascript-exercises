ec4c3c2982d5cc8ff660ec44eb3b0110f6c3d1c5
Devin Beck <devin6beck@gmail.com>
2022-02-03 07:58:42 -0700

    complete hellowWorld and repeatString exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+  if (num < 0) {
+    return "ERROR";
+  }
 
+  let stringToBeReturned = "";
+  for (i = 0; i < num; i++) {
+    stringToBeReturned += string;
+  }
+  return stringToBeReturned;
 };
 
 // Do not edit below this line
