94ee9d4b79cadab632ec2c18d455a2fef9085aa2
Devin Beck <devin6beck@gmail.com>
2022-02-09 14:37:14 -0700

    complete 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -7,21 +7,33 @@ const subtract = function(x, y) {
 };
 
 const sum = function(array) {
-  totalSum = 0;
+  let totalSum = 0;
 	array.forEach(number => totalSum += number);
   return totalSum;
 };
 
-const multiply = function() {
 
+//find better way to do this
+const multiply = function(array) {
+  let total = 1;
+	array.forEach(number => total = total * number);
+  return total;
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return x**y;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let result = num;
+  if (num === 0 || num === 1) {
+    return 1;
+  }
+	while (num > 1) {
+    num--;
+    result = result * num;
+  }
+  return result;
 };
 
 // Do not edit below this line
