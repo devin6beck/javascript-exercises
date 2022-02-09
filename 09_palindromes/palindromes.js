455e6074d551b070259b8030aa38843f3501ed48
Devin Beck <devin6beck@gmail.com>
2022-02-09 16:25:05 -0700

    complete palindrome lesson

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,29 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  // make regular expression to help remove all non Alpha or Numeric charaters
+  const regex = /[^A-Za-z0-9]/g;
 
+  // This will remove all the non-alphanumeric characters from the string and 
+  // replaces it with an empty string. Then remove the spaces.
+  const stringAlphaNumericNoSpaces = string
+                                      .replace(regex, "")
+                                      .split(" ")
+                                      .join("");
+
+  // create a variable that points to a blank string
+  let reverseStringNoSpaces = "";
+
+  // reverse the string with a for loop
+  for (let i = stringAlphaNumericNoSpaces.length - 1; i >= 0; i--) {
+    reverseStringNoSpaces += stringAlphaNumericNoSpaces[i];
+  }
+
+  if (stringAlphaNumericNoSpaces.toLowerCase() === reverseStringNoSpaces
+                                                            .toLowerCase()
+                                                            .split(" ")
+                                                            .join("")) {
+    return true;
+  }
+  return false;
 };
 
 // Do not edit below this line
