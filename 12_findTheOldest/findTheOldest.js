784ac417ec7e3979338d9b8c38e289cf8227e29f
Devin Beck <devin6beck@gmail.com>
2022-02-10 10:55:17 -0700

    change from for loop to .reduce()

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,16 +1,22 @@
 const currentYear = new Date().getFullYear();
 
 const findTheOldest = function(array) {
-  let oldestPerson = array[0];
-  let oldestAge = findAge(array[0]);
 
-  for (let i = 1; i < array.length; i++) {
-    if (oldestAge < findAge(array[i])) {
-      oldestAge = findAge(array[i]);
-      oldestPerson = array[i];
-    }
-  }
-  return oldestPerson
+  return array.reduce((oldest, currentPerson) => {
+    const oldestAge = findAge(oldest);
+    const currentAge = findAge(currentPerson);
+    return oldestAge < currentAge ? currentPerson : oldest
+  })
+  // let oldestPerson = array[0];
+  // let oldestAge = findAge(array[0]);
+
+  // for (let i = 1; i < array.length; i++) {
+  //   if (oldestAge < findAge(array[i])) {
+  //     oldestAge = findAge(array[i]);
+  //     oldestPerson = array[i];
+  //   }
+  // }
+  // return oldestPerson
 };
 
 function findAge(person) {
