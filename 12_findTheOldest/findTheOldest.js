4d94baec3f1465c9e7182ca32ceca4109be3ef4b
Devin Beck <devin6beck@gmail.com>
2022-02-10 07:53:38 -0700

    add function to findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,24 @@
-const findTheOldest = function() {
+const currentYear = new Date().getFullYear;
 
+const findTheOldest = function(array) {
+  let oldestPerson = array[0];
+  let oldestAge = findAge(array[0]);
+
+  for (let i = 1; i < array.length; i++) {
+    if (oldestAge < findAge(array[i])) {
+      oldestAge = findAge(array[i]);
+      oldestPerson = array[i];
+    }
+  }
+  return oldestPerson
 };
+
+function findAge(person) {
+  if (person.yearOfDeath) {
+    return person.yearOfDeath - person.yearOfBirth;
+  }
+  return currentYear - person.yearOfBirth
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
