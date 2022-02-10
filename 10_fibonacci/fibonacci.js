b477874f7b50d2ed51e215453e9c65edaa0b0f8c
Devin Beck <devin6beck@gmail.com>
2022-02-10 07:44:50 -0700

    add "OOPS" if num is negative. COMPLETE

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,10 @@
 const fibonacci = function(num) {
   const fib = [0, 1];
 
+  if (num < 0) {
+    return "OOPS"
+  }
+
   // figure out the fib sequence up to num
   for (i = 2; i <= num; i ++) {
     fib.push(fib[i - 1] + fib[i - 2])
