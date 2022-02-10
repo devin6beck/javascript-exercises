b7a169fd798ceacfd4599e713d16cdf892911b17
Devin Beck <devin6beck@gmail.com>
2022-02-10 07:49:55 -0700

    complete 11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
+const getTheTitles = function(array) {
+  const titlesArray = [];
 
+  array.forEach(book => {
+    titlesArray.push(book.title);
+  });
+  return titlesArray
 };
 
 // Do not edit below this line
