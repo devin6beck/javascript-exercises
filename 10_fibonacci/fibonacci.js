ee354466034d04826fd1f5424396a1eeb8275701
Devin Beck <devin6beck@gmail.com>
2022-02-10 07:42:32 -0700

    first test passed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+  const fib = [0, 1];
 
+  // figure out the fib sequence up to num
+  for (i = 2; i <= num; i ++) {
+    fib.push(fib[i - 1] + fib[i - 2])
+  }
+
+  console.log(`fib: ${fib}`)
+  return fib[num];
 };
 
 // Do not edit below this line
